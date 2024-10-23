90b2d153e155e57dc2bfca451505cbbab87e2312
alfonsotovar67 <alfonso.tovar67@gmail.com>
2024-10-23 16:31:38 -0600

    Se cambia la estructura para empezar a dejar todo en metodos

--- src/main/java/org/sopawebservices/Main.java
+++ src/main/java/org/sopawebservices/Main.java
@@ -1,5 +1,6 @@
 package org.sopawebservices;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,14 +13,24 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Main m = new Main(args);
+    }
 
-        Path path = Paths.get("ruta/del/archivo.txt");
+    public Main(String[] fileName) {
+        getDataFromFile(fileName.length == 0? "" : fileName[0]);
+    }
 
-        try (Stream<String> lineas = Files.lines(path)) {
+    public void getDataFromFile(String fileName) {
+        Path path = Paths.get(fileName);
+        if (!fileName.isEmpty()) {
+            try (Stream<String> lineas = Files.lines(path)) {
 
-            lineas.forEach(linea -> {String[] campos = linea.split("|");});
-        } catch (IOException e) {
-            e.printStackTrace();
+                lineas.forEach(linea -> {String[] campos = linea.split("|");});
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+
         }
     }
 }
